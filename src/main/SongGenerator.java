cd62665b03700f156ddae6ce300f452fe164fadc
Priyanka <81494030+priyankas-world@users.noreply.github.com>
2021-03-27 23:17:43 -0400

    Update SongGenerator.java

--- src/main/SongGenerator.java
+++ src/main/SongGenerator.java
@@ -11,16 +11,16 @@ public class SongGenerator
 {
     private static ResourceBundle filePathAndVariables;
     
-    public static void main(final String[] args) {
+    public static void main(String[] args) {
         System.out.println("Generating a random Lana Del Rey circa 2011-2014 song...");
-        final FileParser fileParser = new FileParser();
+        FileParser fileParser = new FileParser();
         SongGenerator.filePathAndVariables = ResourceBundle.getBundle("FilePathAndVariables");
-        final String pathToSongs = SongGenerator.filePathAndVariables.getString("lanaDelRey");
-        final int songOrder = Integer.valueOf(SongGenerator.filePathAndVariables.getString("defaultSongOrder"));
-        final int songStructureOrder = Integer.valueOf(SongGenerator.filePathAndVariables.getString("defaultSongStructureOrder"));
+        String pathToSongs = SongGenerator.filePathAndVariables.getString("lanaDelRey");
+        int songOrder = Integer.valueOf(SongGenerator.filePathAndVariables.getString("defaultSongOrder"));
+        int songStructureOrder = Integer.valueOf(SongGenerator.filePathAndVariables.getString("defaultSongStructureOrder"));
         try {
-            final SongGeneratorUtils songGeneratorUtils = new SongGeneratorUtils();
-            final String song = songGeneratorUtils.generateFullSong(pathToSongs, songOrder, songStructureOrder, fileParser);
+            SongGeneratorUtils songGeneratorUtils = new SongGeneratorUtils();
+            String song = songGeneratorUtils.generateFullSong(pathToSongs, songOrder, songStructureOrder, fileParser);
             printSong(song);
         }
         catch (FileNotFoundException e) {
@@ -31,11 +31,11 @@ public class SongGenerator
         }
     }
     
-    private static void printSong(final String s) {
-        final String[] words = s.split("\\s");
+    private static void printSong(String s) {
+        String[] words = s.split("\\s");
         int wordsOnLine = 0;
         for (int i = 0; i < words.length; ++i) {
-            final boolean hasProperNoun = Arrays.asList(SongGenerator.filePathAndVariables.getString("properNouns").split(", ")).contains(words[i]);
+            boolean hasProperNoun = Arrays.asList(SongGenerator.filePathAndVariables.getString("properNouns").split(", ")).contains(words[i]);
             if ((words[i].length() >= 1 && Character.isUpperCase(words[i].charAt(0)) && !hasProperNoun) || (hasProperNoun && wordsOnLine > 8) || (words[i].length() > 1 && words[i].equals(words[i].toUpperCase()))) {
                 System.out.println("\n");
                 wordsOnLine = 0;
